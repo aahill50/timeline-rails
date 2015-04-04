d6718bf71409aef0293cf2dcd733176e04767b0a
Aaron Hill <aahill50@gmail.com>
2015-04-04 19:22:45 -0400

    Finish user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,16 @@ class User < ActiveRecord::Base
   
   validates :username, :email, :password_digest, presence: true
   validates :username, :email, :session_token, uniqueness: true
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :password, confirmation: true
 
   after_initialize :ensure_session_token
   
+  def User.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+    
   def ensure_session_token
     self.session_token ||= generate_session_token
   end

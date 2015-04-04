65b9175425b81d323d842782790eb99f1ebc42f8
Aaron Hill <aahill50@gmail.com>
2015-04-04 13:03:56 -0400

    Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimelineRails::Application.config.secret_key_base = '54eff98adcd1c5347e6f937fd0ffa1188441b918c04e1ebd7e9abcae756f33ba80af7e8b2e6d5bc89676cb09a261ec5cf73924d2d2d490c8efbb3ee51d70e9b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TimelineRails::Application.config.session_store :cookie_store, key: '_timeline-rails_session'
